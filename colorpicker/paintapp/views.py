241e091d85b22341dd02150a3003a72b0cedc7cd
Harbir01 <hsingh017@outlook.com>
2022-05-08 14:18:37 -0400

    Updated Views File

--- colorpicker/paintapp/views.py
+++ colorpicker/paintapp/views.py
@@ -6,6 +6,19 @@ from paintapp.forms import ColorPickerForm
 
 # Create your views here.
 class ColorPickerView(View):
+    def get(self, request):
+        """Present the color picker form and color the user"""
+        form = ColorPickerForm()
+
+        context = {
+            'form': form,
+            'red': 255,
+            'green': 255,
+            'blue': 255,
+        }
+
+        return render(request, 'paint.html', context=context)
+
     def post(self, request):
         """Display the user's chosen color"""
         form = ColorPickerForm(request.POST)
@@ -21,3 +34,4 @@ class ColorPickerView(View):
             'blue': blue,
         }
         return render(request, 'paint.html', context=context)
+
